59536e042ca2130e3d84a486f32832c1d95726c6
alexseymourdev <alexseymourdev@gmail.com>
2021-11-11 21:02:20 +0000

    completing tasks from javascript lesson 1

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,21 @@
+console.log('Hello');
+
+//Single line comment
+
+/*--------
+
+Multi line comment
+
+--------*/
+
+let productName = 'alex';
+let price = '10.00';
+let quantity = 3;
+let inStock = true;
+let discountAmount = 0;
+
+console.log(productName);
+console.log(price);
+console.log(quantity);
+console.log(inStock);
+console.log(discountAmount);
